eac6b30717f56b7061b374f6226ca03caf54cfa8
Vladimir Bulatov <vladimir@shapeways.com>
2024-04-10 13:40:43 -0700

    fix build.xml

--- comp.bat
+++ comp.bat
@@ -2,5 +2,5 @@
 rem call ant -emacs clean
 rem call ant -emacs build
 rem 
-rem 
-call ant -emacs runMain -Dclass=pvs.polyhedra.stellation.StellationMain
+rem
+    call ant -emacs runMain -Dclass=pvs.polyhedra.stellation.StellationMain
